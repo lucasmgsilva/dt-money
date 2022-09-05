6fa033cdda4d752eb45c6b2cc172feb1f3449b24
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-09-05 13:10:23 -0300

    Setup do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
+}
+
+export default App
