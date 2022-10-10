89294cb95256d62c3eb3c89922a51e5a64933f2a
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-10-10 19:50:12 -0300

    Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,27 @@
 import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowDown, ArrowUp, CurrencyDollar } from 'phosphor-react'
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary(){
+    const { transactions } = useContext(TransactionsContext);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 'income') {
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+        } else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+        }
+
+        return acc;
+    }, {
+        income: 0,
+        outcome: 0,
+        total: 0
+    })
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -9,7 +29,7 @@ export function Summary(){
                     <span>Entradas</span>
                     <ArrowUp size={32} color="#00b37e"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -17,7 +37,7 @@ export function Summary(){
                     <span>Saídas</span>
                     <ArrowDown size={32} color="#f75a68"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -25,7 +45,7 @@ export function Summary(){
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
