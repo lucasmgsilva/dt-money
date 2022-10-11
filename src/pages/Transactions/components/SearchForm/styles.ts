297c0c8ae1225a96f035b3c32c2c9de206b22484
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-10-11 13:57:26 -0300

    Aplicando React Hook Form e Controlled Components

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -29,13 +29,19 @@ export const SearchFormContainer = styled.form`
         color: ${props => props.theme['green-300']};
         font-weight: bold;
         border-radius: 6px;
+        cursor: pointer;
+
+        &:disabled {
+            opacity: 0.6;
+            cursor: not-allowed;
+        }
         
-        &:hover {
+        &:not(:disabled):hover {
             background: ${props => props.theme['green-500']};
             border-color: ${props => props.theme['green-500']};
             color: ${props => props.theme.white};
 
-            transition: background-color 0.2s, color 0.2s, border-color: 0.2s
+            transition: background-color 0.2s, color 0.2s, border-color 0.2s
         }
     }
 `
