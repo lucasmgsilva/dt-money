b76a5f806a24d4968a237ffc11702c0b9d2a43c5
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-10-12 00:03:25 -0300

    Criando nova transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,9 @@ import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton }
 import * as z from "zod";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { api } from "../../lib/axios";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 const newTransactionFormSchema = z.object({
     description: z.string(),
@@ -15,11 +18,14 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal(){
+    const { createTransaction } = useContext(TransactionsContext)
+
     const {
         control,
         register, 
         handleSubmit,
-        formState: {isSubmitting}
+        formState: {isSubmitting},
+        reset
     } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema),
         defaultValues: {
@@ -28,8 +34,12 @@ export function NewTransactionModal(){
     })
 
     async function handleCreateNewTransaction(data: NewTransactionFormInputs){
-        await new Promise(resolve => setTimeout(resolve, 2000))
-        console.log(data)
+        // await new Promise(resolve => setTimeout(resolve, 2000))
+        const {description, price, category, type} = data
+
+        await createTransaction({description, price, category, type})
+
+        reset();
     }
 
     return (
